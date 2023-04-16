104830eb57c29dc7530c2b2099dece59c6462cff
bikram9657 <bikramjit.singh@gmail.com>
2023-04-15 19:40:40 -0500

    Created basic 'Login' API structure.

---
+++ src/main/java/com/login/loginv1/LoginV1Application.java
@@ -0,0 +1,13 @@
+package com.login.loginv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginV1Application.class, args);
+	}
+
+}
